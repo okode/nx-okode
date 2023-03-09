cb73575011d7f0773fe8b4e1a41ef66f3d20658b
Rubén Panadero <rpanadero@okode.com>
2023-03-09 12:54:16 +0100

    feat: lighthouse-ci

---
+++ e2e/lighthouse-ci-e2e/src/lighthouse-ci.spec.ts
@@ -0,0 +1,39 @@
+import {
+  cleanup,
+  readJson,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
+import { newProject } from '@okode/nx-plugin-testing-devkit';
+
+describe('lighouse ci e2e', () => {
+  const appName = uniq('test');
+  // Setting up individual workspaces per
+  // test can cause e2e runs to take a long time.
+  // For this reason, we recommend each suite only
+  // consumes 1 workspace. The tests should each operate
+  // on a unique project in the workspace, such that they
+  // are not dependant on one another.
+  beforeAll(async () => {
+    newProject(
+      [
+        { name: '@okode/nx-lighthouse-ci', path: 'dist/packages/lighthouse-ci' }
+      ],
+      ['@nrwl/angular']
+    );
+    await runNxCommandAsync(`generate @nrwl/angular:application ${appName} --routing true --style scss --standalone false`);
+    await runNxCommandAsync(`generate @okode/nx-lighthouse-ci:setup ${appName}`);
+  }, 120000);
+
+  afterAll(() => {
+    // `nx reset` kills the daemon, and performs
+    // some work which can help clean up e2e leftovers
+    runNxCommandAsync('reset');
+    cleanup();
+  });
+
+  it('should lighthous ci target exist', () => {
+    const projectConfig = readJson(`apps/${appName}/project.json`);
+    expect(projectConfig.targets['lighthouse-ci-check']).toBeDefined();
+  });
+});
---
+++ packages/lighthouse-ci/README.md
@@ -0,0 +1,11 @@
+# lighthouse-ci
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build lighthouse-ci` to build the library.
+
+## Running unit tests
+
+Run `nx test lighthouse-ci` to execute the unit tests via [Jest](https://jestjs.io).
